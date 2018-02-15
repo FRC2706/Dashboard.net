a33ca1eeaac0afccfdc8ac26c7d7d6b2622d9d92
Kyle Anderson <kra.aue@gmail.com>
2018-02-14 19:19:02 -0500

    First commit
    Repository Set up and ready for coding.

---
+++ Dashboard.net/MainWindow.xaml.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Windows;
+using NetworkTables;
+
+namespace Dashboard.net
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void button_Click(object sender, RoutedEventArgs e)
+        {
+            NetworkTable.SetPort(1735);
+            NetworkTable.SetIPAddress("10.27.6.2");
+            NetworkTable.SetClientMode();
+            NetworkTable.Initialize();
+            NetworkTable smartDashboard = NetworkTable.GetTable("SmartDashboard");
+
+            //while (!smartDashboard.IsConnected)
+            //{
+            //    Console.WriteLine("Robot Not Connected");
+            //}
+
+
+            while (!smartDashboard.IsConnected)
+            {
+
+            }
+
+            Console.WriteLine(smartDashboard.GetString("test", "Null"));
+            smartDashboard.PutString("test2", "hi");
+        }
+
+        private void OnConnectButtonClicked(object sender, RoutedEventArgs e)
+        {
+
+        }
+    }
+}
---
+++ Dashboard.net/SmartDashboard.cs
@@ -0,0 +1,17 @@
+﻿using NetworkTables;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Dashboard.net
+{
+    class SmartDashboard
+    {
+        public SmartDashboard(string connectAddress)
+        {
+            NetworkTable.
+        }
+    }
+}
