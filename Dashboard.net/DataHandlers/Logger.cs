49434e69e8a93adf8fe54519fb72413f5b53950e
= <kra.aue@gmail.com>
2018-04-02 18:38:02 -0400

    Initial Logging Commit This branch will be for the development of the logging feature, which will be used for debugging problems with the dashboard if and when then occur.

---
+++ Dashboard.net/DataHandlers/Logger.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Dashboard.net.DataHandlers
+{
+    /// <summary>
+    /// Class to be used to log application data
+    /// </summary>
+    public class Logger
+    {
+        /// <summary>
+        /// The current instance of the logger class
+        /// </summary>
+        public static Logger currentInstance;
+
+        public Logger()
+        {
+            currentInstance = this;
+        }
+    }
+}
